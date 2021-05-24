edd6fa1a3f0702328d5904f255e64afcba26b111
ali mahdavi <a5mahdavy5@gmail.com>
2021-05-24 21:32:47 +0430

    better ci cleanup logs

--- src/Keys.test.ts
+++ src/Keys.test.ts
@@ -4,12 +4,8 @@ import { GenerateSecretKey } from "./Keys"
 
 
 describe("[keys]", () => {
-    // beforeAll(async () => {
-
-    // })
     it("Should generate secret key",async(done)=>{
         let secret=await GenerateSecretKey()
-        // console.log(secret);
         done()
     })
 })
